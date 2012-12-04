e8428fe5fe900e832b379c0cefb9b5bbe9142e62
ErwanLP <erwan.le-poder@isep.fr>
2012-12-04 11:40:18 +0100

    init

--- section.php
+++ section.php
@@ -1,14 +1,12 @@
 <?php
-if (!empty($_SESSION['ID'])) {
+if (empty($_SESSION['ID'])) {
     ?>
     <section>
         <aside class ="new">
-            <div class="image">
-                <p class="blanc"> Bonjour chèrs internautes ! Voici les dernières news du site :<br />
-                <table image="2"> <tr> <td> <marquee> <a href="page2.php"> Ce soir le Cabaret Le Moulin Rouge [..]</a> <a href="page3.php"> Au cercle d'or, spectacle exceptionnel [..]</a> </marquee> </td> </tr> </table> <!-- ici dynamique les dernières news s'affichent ici --> 
+            
+                <p> Bonjour chèrs internautes ! Voici les dernières news du site :
                 </p>
-            </div>
-        </aside>
+              </aside>
 
         <aside class ="navg">
             <?php include ("arbre.php"); ?>
