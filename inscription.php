a1d3009b1a9eed3d255899743a553d1aacc263c2
ErwanLP <erwan.le-poder@isep.fr>
2012-12-10 16:39:29 +0100

    init

--- inscription.php
+++ inscription.php
@@ -47,15 +47,10 @@
                   </div> -->
                 <div id="formContainer">
                     <form id="login" method="post" action="./">
-                        <a href="#" id="flipToRecover" class="flipLink">Forgot?</a>
+                       
                         <input type="text" name="loginEmail" id="loginEmail" value="Email" />
                         <input type="password" name="loginPass" id="loginPass" value="pass" />
                         <input type="submit" name="submit" value="Login" />
-                    </form>
-                    <form id="recover" method="post" action="./">
-                        <a href="#" id="flipToLogin" class="flipLink">Forgot?</a>
-                        <input type="text" name="recoverEmail" id="recoverEmail" value="Email" />
-                        <input type="submit" name="submit" value="Recover" />
                     </form>
                 </div>
                 <!-- JavaScript includes -->
