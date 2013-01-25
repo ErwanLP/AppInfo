f0f4ff2eb0431bfc99ea9f9a5a5dde68f27e76a4
ErwanLP <erwan.le-poder@isep.fr>
2013-01-25 10:06:11 +0100

    init

--- traitementSignalement.php
+++ traitementSignalement.php
@@ -1,20 +1,16 @@
 <?php
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+$idSignaleur = $_POST['IDsignaleur'];
+$idMessage = $_POST['IDmessage'];
+$motif = $_POST['motif'];
+
+echo $idSignaleur;
+echo "<br/>";
+echo $idMessage;
+echo "<br/>";
+echo $motif;
+
+
+
+
 ?>
 
-
-<form method="post" action="traitement.php">
-    <select name="motif" id="motif">
-        <option value="0">Motif</option>
-        <option value="1">Injurieux</option>
-        <option value="2">Raciste</option>
-        <option value="3">Homophobe</option>
-        <option value="4">Flood</option>
-    </select>    
-    <input type="hidden" value="IDsignaleur" />
-    <input type="hidden" value="IDmessage" />
-    <input type="submit" value="Signaler" />
-</form>
